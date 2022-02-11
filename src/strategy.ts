9b294ed8a11a8f5a3b4c05ad5a3f0a7dad184c68
Iryna Anikina <iriinademina@gmail.com>
2022-02-11 18:25:05 +0200

    add implementation strategy pattern

---
+++ src/strategy.ts
@@ -0,0 +1,120 @@
+export interface FlyBehanior {
+  fly: () => void;
+}
+
+export interface QuackBehavior {
+  quack: () => void;
+}
+
+// concrete strategy quack behavior classes
+export class Quack implements QuackBehavior {
+  public quack() {
+    console.log("Ouack");
+  }
+}
+
+export class MuteQuack implements QuackBehavior {
+  public quack() {
+    console.log("MuteOuack");
+  }
+}
+
+export class SqueakQuack implements QuackBehavior {
+  public quack() {
+    console.log("SqueakQuack");
+  }
+}
+
+// concrete strategy fly behavior classes
+export class FlyWithWings implements FlyBehanior {
+  public fly() {
+    console.log("Fly");
+  }
+}
+
+export class FlyNoWay implements FlyBehanior {
+  public fly() {
+    console.log("I can't fly");
+  }
+}
+
+export class FlyRocketPowered implements FlyBehanior {
+  public fly() {
+    console.log("I am flying with a rocket");
+  }
+}
+
+export abstract class Duck {
+  constructor(
+    private flyBehavior: FlyBehanior,
+    private quackBehavior: QuackBehavior
+  ) {
+    this.flyBehavior = flyBehavior;
+    this.quackBehavior = quackBehavior;
+  }
+
+  public abstract display(): void;
+
+  public performFly(): void {
+    this.flyBehavior.fly();
+  }
+
+  public performQuack(): void {
+    this.quackBehavior.quack();
+  }
+
+  public swim(): void {
+    console.log("All ducks float!");
+  }
+
+  public setFlyBehavior(flyBehavior: FlyBehanior) : void {
+    this.flyBehavior = flyBehavior;
+  }
+
+  public setQuackBehavior(quackBehavior: QuackBehavior) : void {
+    this.quackBehavior = quackBehavior;
+  }
+}
+
+export class MallardDuck extends Duck {
+  constructor(
+    flyBehavior: FlyBehanior = new FlyWithWings(),
+    quackBehavior: QuackBehavior = new Quack()
+  ) {
+    super(flyBehavior, quackBehavior);
+  }
+
+  public display(): void {
+    console.log("I'm a Mallard duck");
+  }
+}
+
+export class ModelDuck extends Duck {
+  constructor(
+    flyBehavior: FlyBehanior = new FlyNoWay(),
+    quackBehavior: QuackBehavior = new SqueakQuack()
+  ) {
+    super(flyBehavior, quackBehavior);
+  }
+
+  public display(): void {
+    console.log("I'm a model duck");
+  }
+}
+
+export class MiniDuckSimulator {
+  public static main(): void {
+    let mallard: Duck;
+
+    mallard = new MallardDuck();
+    mallard.performQuack();
+    mallard.performFly();
+    mallard.display();
+
+    let model = new ModelDuck();
+    model.performFly();
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
+    model.swim();
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports ={
+    mode: 'development',
+    entry: './src/app.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+         title: 'Output Management',
+         title: 'Development',
+        }),
+      ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    }, 
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+      },
+    module : {
+        rules: [
+            {
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
